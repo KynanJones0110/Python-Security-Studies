64d8fd0dc16b2323f7f5c776bcc16bf059b58432
KynanJones0110 <71669145+KynanJones0110@users.noreply.github.com>
2023-06-06 00:45:50 +0100

    Update AbuseIPDB-Bulk.py
    Added yeet and run error handling

--- AbuseIPDB-Bulk.py
+++ AbuseIPDB-Bulk.py
@@ -2,6 +2,7 @@ import requests
 import json
 import pandas
 import csv
+import sys
 
 #If you're using office online you can only export as excel docs as .xlsx, I'd suggest just using Google sheets :)
 
@@ -11,15 +12,23 @@ IP_CSV = pandas.read_csv((file_path))
 ip=IP_CSV["IP"].tolist()
 
 #You will need to create a free account for this
-API_KEY = "ENTER YOUR API"
+API_KEY = "ENTER API KEY"
 url = 'https://api.abuseipdb.com/api/v2/check'
-
+validate_key = len(API_KEY)
 csv_columns = ['ipAddress','isPublic','ipVersion','isWhitelisted','abuseConfidenceScore','countryCode','usageType','isp','domain','hostnames','totalReports','numDistinctUsers','lastReportedAt']
 
 headers = {
     'Accept': 'application/json',
     'Key': API_KEY
 }
+# check key 
+if validate_key < 13: #change for better handling sometime
+    print("You need to enter your API key, the variable name is 'API_KEY'")
+    sys.exit()
+else:
+    print("API key is valid. Continuing with the program...")
+
+
 with open("AbuseIP_results.csv","a", newline='') as filecsv:
     writer = csv.DictWriter(filecsv, fieldnames=csv_columns)
     writer.writeheader()
